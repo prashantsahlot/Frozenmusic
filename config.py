1d271e31e6e66dbfe99620707959d2d2963d1a38
Frozen owner <156554454+prashantsahlot@users.noreply.github.com>
2024-03-29 17:34:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 API_ID = int(os.environ.get('26495505'))
 API_HASH = os.environ.get('41ce1aa633c6ae79b928ddd4caefc245')
-BOT_TOKEN = os.environ.get('')
+BOT_TOKEN = os.environ.get('6459410232:AAGkW4xupDFkphxwr5bLkC_e_c6YPd_sSJw')
 YOUTUBE_API_KEY = os.environ.get('AIzaSyAqJOx0P-yh3kbtaIYRsCTrO6QMTa8SVhY')
