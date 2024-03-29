6abdae0ed2043c553696dbdb7586625e8b2d1131
Frozen owner <156554454+prashantsahlot@users.noreply.github.com>
2024-03-29 17:11:56 +0530

    Update

---
+++ config.py
@@ -0,0 +1,4 @@
+API_ID = int(os.environ.get('TELEGRAM_API_ID'))
+API_HASH = os.environ.get('TELEGRAM_API_HASH')
+BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+YOUTUBE_API_KEY = os.environ.get('YOUTUBE_API_KEY')
